05f591d2c8499bd544151a1ea85766e60eefbf0c
AndresYepesM <aymoreno567@gmail.com>
2021-05-26 15:35:30 -0400

    Bugs Fixed, Service initialized

--- readytomine/urls.py
+++ readytomine/urls.py
@@ -4,9 +4,15 @@ from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
+
+	# Django administration urls
     path('admin/', admin.site.urls),
 
+
+    # Django authentication urls
     path('accounts/', include('django.contrib.auth.urls')),
 
+
+    # Users administration site
     re_path('', include('user.urls')),
 ]
--- user/urls.py
+++ user/urls.py
@@ -34,14 +34,4 @@ urlpatterns = [
 
     # mensaje de nuevo ususario
     path('Usuario_eliminado', views.delete_userMsj, name='ususario_eliminado'),
-
-    # Reset Password urls
-
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
-
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='"password_reset_done'),
-
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
