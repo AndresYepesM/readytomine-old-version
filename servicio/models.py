0e29d501aa3ea4814ed5a08e9cd3f8ec82723a28
AndresYepesM <aymoreno567@gmail.com>
2021-07-13 17:01:36 -0400

    QR code disabled, and feedback fixed

--- servicio/models.py
+++ servicio/models.py
@@ -41,23 +41,23 @@ class Pedido(models.Model):
 	fecha_taller = models.DateField(null=True, blank=True)
 	fecha_egreso = models.DateField(null=True, blank=True)
 	partes = models.ManyToManyField(Partes)
-	qr_code = models.ImageField(upload_to='qr_codes', blank=True)
+	#qr_code = models.ImageField(upload_to='qr_codes', blank=True)
 
 	class Meta:
 		ordering=["-id"]
 		verbose_name_plural="Pedidos"
 
-	def __str__(self):
-		return '{} {}'.format(self.fecha_ingreso, self.persona)
+	#def __str__(self):
+	#	return '{} {}'.format(self.fecha_ingreso, self.persona)
 
-	def save(self, *args, **kwargs):
-		qrcode_img = qrcode.make(self.orden_pedido)
-		canvas = Image.new('RGB', (300, 300), '#ffffff')
-		draw = ImageDraw.Draw(canvas)
-		canvas.paste(qrcode_img)
-		fname = f'qr_code-{self.orden_pedido}'+'.png'
-		buffer = BytesIO()
-		canvas.save(buffer,'PNG')
-		self.qr_code.save(fname, File(buffer), save=False)
-		canvas.close()
-		super().save(*args, **kwargs)
+	#def save(self, *args, **kwargs):
+	#	qrcode_img = qrcode.make(self.orden_pedido)
+	#	canvas = Image.new('RGB', (300, 300), '#ffffff')
+	#	draw = ImageDraw.Draw(canvas)
+	#	canvas.paste(qrcode_img)
+	#	fname = f'qr_code-{self.orden_pedido}'+'.png'
+	#	buffer = BytesIO()
+	#	canvas.save(buffer,'PNG')
+	#	self.qr_code.save(fname, File(buffer), save=False)
+	#	canvas.close()
+	#	super().save(*args, **kwargs)
